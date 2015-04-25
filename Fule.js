6ba7574edf059068e5753390a37c0ea8b0f6329f
Woust <woustw@mail.ru>
2015-04-25 16:16:18 +0300

    1. First commit

---
+++ Fule.js
@@ -0,0 +1,33 @@
+var http=require('http');
+
+var server=http.createServer(function(request, response) {
+
+response.writeHead(200, {'Content-Type': 'text/plain'})
+
+response.end('hello world');
+
+});
+
+server.listen(8000);
+
+console.log('server started');
+Это создание сервера
+var http=require('http');
+
+var fs=require('fs');
+
+var server=http.createServer(function(request, response) {
+
+response.writeHead(200, {'Content-Type': 'text/html'})
+
+fs.readFile('C:/Users/Landread/WebstormProjects/untitled/index.html', function(error, contents){
+
+response.end(contents);
+
+})
+
+});
+
+server.listen(8000);
+
+console.log('server started');
