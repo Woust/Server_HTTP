3eb201b447e812e251e832315acb97ce07e98963
Woust <woustw@mail.ru>
2015-04-25 19:44:15 +0300

    Update Fule.js

--- Fule.js
+++ Fule.js
@@ -1,17 +1,4 @@
-var http=require('http');
-
-var server=http.createServer(function(request, response) {
-
-response.writeHead(200, {'Content-Type': 'text/plain'})
-
-response.end('hello world');
-
-});
-
-server.listen(8000);
-
-console.log('server started');
-Это создание сервера
+	
 var http=require('http');
 
 var fs=require('fs');
